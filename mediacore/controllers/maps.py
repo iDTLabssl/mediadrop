b7fa9312490bcfc8321f0c5d6ac6d187e4f33fe1
Samson Paul Jnr <hello@samsonpaul.me>
2017-11-04 15:21:23 +0100

    updates map controller

--- mediacore/controllers/maps.py
+++ mediacore/controllers/maps.py
@@ -1,11 +1,8 @@
-from pylons import request, response, tmpl_context as c
-from pylons.controllers.util import abort
-from sqlalchemy import orm
-
 from mediacore.lib.base import BaseController
 from mediacore.lib.decorators import (expose, observable)
 from mediacore.plugin import events
 
+import googlemaps
 import logging
 log = logging.getLogger(__name__)
 
@@ -14,6 +11,14 @@ class MapController(BaseController):
     """
         Map Controller
     """
+
+    gmaps = googlemaps.Client(key='AIzaSyA3mWehI-WUOmQlTs7h5sV339EcL484DHQ')
+
+    # Geocoding an address
+    geocode_result = gmaps.geocode('Siera Leone & Liberia')
+
+    # Look up an address with reverse geocoding
+    reverse_geocode_result = gmaps.reverse_geocode((7.493196470122287, -10.1953125))
 
     @expose('/map.html')
     @ observable(events.MapsController.mapping)
