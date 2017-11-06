c49730a0fb3046749538b50a0e6e3dedb8a44d07
Samson Paul Jnr <hello@samsonpaul.me>
2017-11-06 14:42:15 +0100

    [UPDATE] upload field

--- mediacore/forms/admin/media.py
+++ mediacore/forms/admin/media.py
@@ -16,7 +16,7 @@ from tw.forms.validators import Int, DateTimeConverter, FieldStorageUploadConver
 from mediacore.lib import helpers
 from mediacore.lib.filetypes import registered_media_types
 from mediacore.lib.i18n import N_, _, get_available_locales
-from mediacore.forms import FileField, Form, ListForm, SubmitButton, TextArea, TextField, XHTMLTextArea, MapButton, email_validator
+from mediacore.forms import FileField, Form, ListForm, SubmitButton, TextArea, TextField, XHTMLTextArea, email_validator
 from mediacore.forms.admin.categories import CategoryCheckBoxList
 from mediacore.model import Category, DBSession, Podcast
 from mediacore.forms.admin.settings import languages
@@ -153,7 +153,6 @@ class MediaForm(ListForm):
         CategoryCheckBoxList('categories', label_text=N_('Categories'), options=lambda: DBSession.query(Category.id, Category.name).all()),
         TextArea('tags', label_text=N_('Tags'), attrs=dict(rows=3, cols=15), help_text=N_(u'e.g.: land, sports, economy')),
         SingleSelectField('location', label_text=N_('Location'), options=lambda: [('sierra-leone', 'Siera Leone'), ('liberia', 'Liberia')]),
-        MapButton('map', default=N_('Get Your Coordinates'), named_button=True),
         SingleSelectField('language', label_text=N_('Language'), options=languages),
         XHTMLTextArea('description', label_text=N_('Description'), attrs=dict(rows=5, cols=25)),
         TextArea('notes',
